8763fbb652db5edc0c72e5dcc4e728f8ac7b07db
Rob Sonke <r.sonke@maxxton.com>
2017-01-07 10:04:18 +0100

    updates

--- brew/brew-cask.sh
+++ brew/brew-cask.sh
@@ -13,10 +13,12 @@ brew cask install franz
 brew cask install garmin-express
 brew cask install go2shell
 brew cask install google-chrome
+brew cask install google-play-music-desktop-player
 brew cask install hyperswitch
 brew cask install istat-menus
 brew cask install iterm2
 brew cask install java
+brew cask install jumpshare
 brew cask install keepassx
 brew cask install launchcontrol
 brew cask install mamp
@@ -31,8 +33,11 @@ brew cask install soundnode
 brew cask install sourcetree
 brew cask install spectacle
 brew cask install spotify
+brew cask install sublime-text
 brew cask install teamviewer
 brew cask install the-unarchiver
 brew cask install vagrant
 brew cask install vagrant-manager
+brew cask install virtualbox
 brew cask install vlc
+brew cask install vox
